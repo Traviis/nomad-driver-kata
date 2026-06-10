7e15d3662be5068304f183a96d3f88a9019dd122
Travis Taylor <travis@trickfire.com>
2026-06-10 12:46:29 -0600

    chore: project scaffolding and Go module setup

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	log "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/nomad/plugins"
+
+	"github.com/trickfire/nomad-driver-kata/kata"
+)
+
+func main() {
+	plugins.Serve(factory)
+}
+
+func factory(logger log.Logger) interface{} {
+	return kata.NewDriver(logger)
+}
